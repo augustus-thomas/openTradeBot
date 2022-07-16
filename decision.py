eb524fe27ce7f907a88a56e749794b76f445f719
Augustus Thomas <33500951+augustus-thomas@users.noreply.github.com>
2022-07-16 13:20:41 -0500

    reformatted file structure

---
+++ decision.py
@@ -0,0 +1,15 @@
+def __init__(self):
+    return self
+
+def __main__():
+    decision = {
+        "asset" : str,
+        "entry-price" : float,
+        "exit-price": float,
+        "certainty" : float
+    }
+    print(decision)
+    return decision
+
+def __name__():
+    return "decision"
