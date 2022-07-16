2500a6ae5b62f357c05c844002684de1484e61f4
Augustus Thomas <33500951+augustus-thomas@users.noreply.github.com>
2022-07-16 15:32:44 -0500

    outlined decision.py

--- decision.py
+++ decision.py
@@ -1,16 +1,23 @@
 class Decision:
     def __init__(self):
         self.list = {
-            "ticker" : "test",
-            "entry-price" : "test",
-            "exit-price": "test",
-            "certainty" : "test",
+            "ticker" : "TEST",
+            "entry-price" : 0,
+            "exit-price" : 0,
+            "certainty" : 0,
         }
 
         self.test_var = "test_var"
 
-    def __main__(self):
-        return self.test_var
+    def rating(self, ticker="", historical_data={}, **entry_params):
+        certainty = "0"
+        return certainty
 
+    def buy_condition():
+        pass
+
+    def sell_condition():
+        pass
+    
     def __name__(self):
         return self.test_var
